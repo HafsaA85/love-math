a62316414819c42b9610e5efdb539bed98dcaae4
HafsaA85 <hafsaadeel4@gmail.com>
2025-08-14 17:06:09 +0100

    JS

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+function runGame(){
+    // This function will start the game, initialize variables, and set up the first question.
+    // It will also handle the game loop and user interactions.
+    document.getElementById('answer-box').value = '';
+    document.getElementById('answer-box
+}
+
+
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+    // This function will calculate the correct answer based on the current question
+    // and return it. It will be used to check the user's answer.
+}
+
+function incrementScore(){
+    // This function will increment the user's score by 1.
+}
+
+function incrementWrongScore(){
+    // This function will increment the user's wrong score by 1.
+}
+
+function displayAdditionQuestion(){
+    // This function will display an addition question to the user.
+    // It will generate two random numbers, display them, and set up the answer box.
+}
+
+function displaysubtractionQuestion(){
+    // This function will display a subtraction question to the user.
+    // It will generate two random numbers, display them, and set up the answer box.
+}
+
+function displayMultiplicationQuestion(){
+    // This function will display a multiplication question to the user.
+    // It will generate two random numbers, display them, and set up the answer box.
+}
